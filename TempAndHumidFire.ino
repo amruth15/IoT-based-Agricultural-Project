21c3251f7e097324576ebdddf29ec78478b0152e
AMRUTH M <63469678+amruth15@users.noreply.github.com>
2020-09-12 09:52:24 +0530

    TempAndHumid

--- TempAndHumidFire.ino
+++ TempAndHumidFire.ino
@@ -1,13 +1,13 @@
 #include <FirebaseArduino.h>
 #include <Firebase.h>
-#include <ESP8266WiFi.h>    //esp8266 library
-#include <DHT.h>         // dht11 temperature and humidity sensor library
+#include <ESP8266WiFi.h>    
+#include <DHT.h>         
 #define FIREBASE_HOST "temphumid-bd7f8.firebaseio.com/"
 #define FIREBASE_AUTH "DF9VdtfpVKgd906sOc5xUbNpJYklqyH5eawD5g3S"
-#define WIFI_SSID "Redmi"                  // wifi name 
-#define WIFI_PASSWORD "11223344"                 //password of wifi
-#define DHTPIN D4                // what digital pin we're connected to
-#define DHTTYPE DHT11              // select dht type as DHT 11 or DHT22
+#define WIFI_SSID "Redmi"                
+#define WIFI_PASSWORD "11223344"             
+#define DHTPIN D4              
+#define DHTTYPE DHT11            
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -24,30 +24,28 @@ void setup() {
   Serial.print("Connected to ");
   Serial.println(WIFI_SSID);
   Serial.print("IP Address is : ");
-  Serial.println(WiFi.localIP());                  //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     // connect to firebase
-  dht.begin();                            //Start reading dht sensor
-  // Firebase.setString("Humidity","0");
-  // Firebase.setString("Temperature","0");
+  Serial.println(WiFi.localIP());                
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);   
+  dht.begin();                     
 }
 
 void loop() {
-  float h = dht.readHumidity();       // Reading temperature or humidity
-  float t = dht.readTemperature();   // Read temperature as Celsius (the default)
+  float h = dht.readHumidity();       
+  float t = dht.readTemperature();   
   delay(20000);
-  if (isnan(h) || isnan(t)) {  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {  
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
   Serial.print("Humidity: ");  Serial.print(h);
-  String fireHumid = String(h) + String("%");    //convert integer humidity to string humidity
+  String fireHumid = String(h) + String("%");  
   Serial.print("%  Temperature: ");  Serial.print(t);  Serial.println("Â°C ");
-  String fireTemp = String(t) + String("Â°C"); //convert integer temperature to string temperature
+  String fireTemp = String(t) + String("Â°C"); 
  
  //  Firebase.setString("Msg","Hello");
-  Firebase.pushString("Humidity", fireHumid);         //setup path and send readings
-  Firebase.pushString("Temperature", fireTemp);        //setup path and send readings
+  Firebase.pushString("Humidity", fireHumid);        
+  Firebase.pushString("Temperature", fireTemp);   
    
 
   Firebase.setFloat("Humidity",h);
